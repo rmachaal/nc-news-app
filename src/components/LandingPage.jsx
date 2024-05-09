57ed84bf6479215e7598ad38b59772b6e9960671
rmachaal <rahafam@hotmail.co.uk>
2024-05-09 14:40:06 +0100

    Completes task 11.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,19 +1,45 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ArticleBlock from "./ArticleBlock";
 
 function LandingPage({ articles, setArticles }) {
+  const [filter, setFilter] = useState("created_at");
+  const [order, setOrder] = useState("DESC");
+
   useEffect(() => {
     axios
-      .get("https://news-api-project-hj1l.onrender.com/api/articles")
+      .get(
+        `https://news-api-project-hj1l.onrender.com/api/articles?sort_by=${filter}&&order=${order}`
+      )
       .then((response) => {
         const { articles } = response.data;
         setArticles(articles);
       });
-  }, []);
+  }, [filter, order]);
+
+  function handleFilter(e) {
+    setFilter(e.target.value);
+  }
+
+  function handleOrder(e) {
+    setOrder(e.target.value);
+  }
 
   return (
     <>
+      <div className="article-filter">
+        <label htmlFor="article-filter">Filter</label>
+        <select id="article-filter" value={filter} onChange={handleFilter}>
+          <option value="created_at">date</option>
+          <option value="comment_count">comment count</option>
+          <option value="votes">votes</option>
+        </select>
+        <label htmlFor="order">Order</label>
+        <select id="order" value={order} onChange={handleOrder}>
+          <option value="DESC">descending</option>
+          <option value="ASC">ascending</option>
+        </select>
+      </div>
       <ul>
         {articles.map((article, index) => {
           return <ArticleBlock key={index} article={article} />;
